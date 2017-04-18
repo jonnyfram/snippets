66f4adb417e68f0f2dc355b68f8db6fefe42a3af
Jonny Fram <jonnyfram@gmail.com>
2017-04-18 12:19:05 +0000

    updating snippets.py

--- snippets.py
+++ snippets.py
@@ -3,3 +3,24 @@ import logging
 #Set the log output file, and the log level
 
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
+
+def put(name , snippet):
+    """
+    Store a snippet with an associated name.Store
+    Returns the name and the snippet
+    """
+    
+    logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
+    return name, snippet
+    
+def get(name):
+    """ Retrieve the snippet with a given name.ArithmeticError
+    
+    If there is no such snippet, return '404: Snippet Not Found'.If
+    
+    Returns the snippet.
+    """
+    logging.error("FIXME: Unimplemented - get({!r})".format(name))
+    return ""
+    
+    
